34395290935a387efcd7dbf564e9bd14773dc5a1
emgixiii <emgi@live.in>
2025-09-02 21:24:19 +0530

    fix: sy function

--- zsh/.zsh/36-sioyek.zsh
+++ zsh/.zsh/36-sioyek.zsh
@@ -1,3 +1,4 @@
 function sy() {
-  /mnt/c/Program\ Files\ \(x86\)/sioyek/sioyek.exe "$@"
+  /mnt/c/Program\ Files\ \(x86\)/sioyek/sioyek.exe "$@" &
+  disown
 }
--- zsh/.zsh/50-aliases.zsh
+++ zsh/.zsh/50-aliases.zsh
@@ -1,6 +1,6 @@
-alias nv="nvim"                  # Neovim
-alias ':e'="nvim"                # Neovim
-alias gg="lazygit"               # Git TUI
-alias tt="taskwarrior-tui"       # Task Manager TUI
-alias bt="btop"                  # Resource Manager TUI
-alias ':q!'="wsl.exe --shutdown" # WSL Shutdown linux shortcut
+alias nv="nvim"                 # Neovim
+alias ':e'="nvim"               # Neovim
+alias gg="lazygit"              # Git TUI
+alias tt="taskwarrior-tui"      # Task Manager TUI
+alias bt="btop"                 # Resource Manager TUI
+alias ':q'="wsl.exe --shutdown" # WSL Shutdown linux shortcut
